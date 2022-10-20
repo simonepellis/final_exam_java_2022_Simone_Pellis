1b9b29b530395b240aba201945cadc9c4e124c60
simonepellis <106583635+simonepellis@users.noreply.github.com>
2022-10-20 09:41:44 +0200

    initial commit

---
+++ SimonePellisEsameFinale/src/main/java/final_exam/SimonePellisEsameFinale/SimonePellisEsameFinaleApplication.java
@@ -0,0 +1,13 @@
+package final_exam.SimonePellisEsameFinale;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimonePellisEsameFinaleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimonePellisEsameFinaleApplication.class, args);
+	}
+
+}
---
+++ SimonePellisEsameFinale/src/test/java/final_exam/SimonePellisEsameFinale/SimonePellisEsameFinaleApplicationTests.java
@@ -0,0 +1,13 @@
+package final_exam.SimonePellisEsameFinale;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimonePellisEsameFinaleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
